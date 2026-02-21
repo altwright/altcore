7b59b1fda25e8a61dd9953b525e40ce1ab6c3077
Benjamin Wright <allwrightyben@proton.me>
2026-02-21 11:29:45 +0800

    Redefines hashmap realloc and free

--- hashmap.c
+++ hashmap.c
@@ -3,6 +3,17 @@
 //
 
 #include "hashmap.h"
+#include "malloc.h"
+
+#ifdef STBDS_REALLOC
+#undef STBDS_REALLOC
+#define STBDS_REALLOC(context, ptr, size) alt_realloc(ptr, size)
+#endif
+
+#ifdef STBDS_FREE
+#undef STBDS_FREE
+#define STBDS_FREE(context, ptr) alt_free(ptr)
+#endif
 
 #define STB_DS_IMPLEMENTATION
 #include "libs/stb_ds.h"
