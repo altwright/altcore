a78ac7b90ea052ca04b40dd81dd563f121eb874f
Benjamin Wright <allwrightyben@proton.me>
2026-05-06 21:21:11 +0800

    Creates event queues.

--- events.c
+++ events.c
@@ -6,12 +6,21 @@
 
 #include <assert.h>
 
+#include "memory.h"
 #include "SDL3/SDL_init.h"
 
 static bool g_events_initd = false;
 static EventSourceFlags g_event_source_flags = 0;
 
-void events_init(EventSourceFlags event_sources) {
+typedef struct EVENTS_QUEUE_T {
+    Event* data;
+    i32 len;
+    i32 cap;
+} EventsQueue;
+
+static EventsQueue g_events_arrays[EVENT_SOURCE_COUNT] = {};
+
+void events_init(const EventInitInfo *info) {
     if (g_events_initd) {
         return;
     }
@@ -19,18 +28,48 @@ void events_init(EventSourceFlags event_sources) {
     bool success = SDL_InitSubSystem(SDL_INIT_EVENTS);
     assert(success);
 
-    g_event_source_flags = event_sources;
+    g_event_source_flags = info->sources;
+
+    for (i32 source_idx = 0; source_idx < EVENT_SOURCE_COUNT; source_idx++) {
+        EventSourceFlag source_flag = 1ULL << source_idx;
+
+        EventsQueue* events_queue = &g_events_arrays[source_idx];
+
+        if (g_event_source_flags & source_flag) {
+            events_queue->data = alt_calloc(
+                info->event_queue_cap,
+                sizeof(*g_events_arrays[source_idx].data)
+            );
+
+            events_queue->cap = info->event_queue_cap;
+        } else {
+            events_queue->data = nullptr;
+            events_queue->cap = 0;
+        }
+
+        events_queue->len = 0;
+    }
 
     g_events_initd = true;
 }
 
 void events_poll() {
-
 }
 
 void events_uninit() {
     if (!g_events_initd) {
         return;
+    }
+
+    for (i32 source_idx = 0; source_idx < EVENT_SOURCE_COUNT; source_idx++) {
+        EventsQueue* events_array = &g_events_arrays[source_idx];
+        if (events_array->data) {
+            alt_free(events_array->data);
+        }
+
+        events_array->data = nullptr;
+        events_array->len = 0;
+        events_array->cap = 0;
     }
 
     g_event_source_flags = 0;
--- events.h
+++ events.h
@@ -7,9 +7,9 @@
 
 #include "draw/window.h"
 
-typedef enum EVENT_SOURCE_FLAG_OPTION_E {
-#ifndef X_EVENT_SOURCE_FLAG_OPTIONS
-#define X_EVENT_SOURCE_FLAG_OPTIONS \
+typedef enum EVENT_SOURCE_E {
+#ifndef X_EVENT_SOURCES
+#define X_EVENT_SOURCES \
     X(KEYBOARD) \
     X(MOUSE) \
     X(WINDOW) \
@@ -18,27 +18,83 @@ typedef enum EVENT_SOURCE_FLAG_OPTION_E {
     X(COUNT)
 #endif
 #ifndef X
-#define X(option) \
-    EVENT_SOURCE_FLAG_OPTION_##option,
+#define X(source) \
+    EVENT_SOURCE_##source,
 #endif
-    X_EVENT_SOURCE_FLAG_OPTIONS
+    X_EVENT_SOURCES
 #undef X
-} EventSourceFlagOption;
+} EventSource;
 
 typedef enum EVENT_SOURCE_FLAG_E : u64 {
 #ifndef X
-#define X(option) \
-    EVENT_SOURCE_FLAG_##option = 1ULL << EVENT_SOURCE_FLAG_OPTION_##option,
+#define X(source) \
+    EVENT_SOURCE_FLAG_##source = 1ULL << EVENT_SOURCE_##source,
 #endif
-    X_EVENT_SOURCE_FLAG_OPTIONS
+    X_EVENT_SOURCES
 #undef X
 } EventSourceFlag;
 
 typedef u64 EventSourceFlags;
 
-void events_init(EventSourceFlags event_sources);
+typedef struct EVENT_INIT_INFO_T {
+    EventSourceFlags sources;
+    i32 event_queue_cap;
+} EventInitInfo;
+
+typedef enum WINDOW_EVENT_TYPE_E {
+#ifndef X_WINDOW_EVENT_TYPES
+#define X_WINDOW_EVENT_TYPES \
+    X(CLOSE) \
+    X(RESIZE) \
+    X(COUNT)
+#endif
+#ifndef X
+#define X(type) \
+    WINDOW_EVENT_##type,
+#endif
+    X_WINDOW_EVENT_TYPES
+#undef X
+} WindowEventType;
+
+typedef struct WINDOW_EVENT_T {
+    WindowEventType type;
+} WindowEvent;
+
+typedef struct KEYBOARD_EVENT_TYPE_E {
+#ifndef X_KEYBOARD_EVENT_TYPES
+#define X_KEYBOARD_EVENT_TYPES \
+    X(KEY_PRESS) \
+    X(KEY_RELEASE) \
+    X(COUNT)
+#endif
+#ifndef X
+#define X(type) \
+    KEYBOARD_EVENT_##type,
+#endif
+#undef X
+} KeyboardEventType;
+
+typedef struct KEYBOARD_EVENT_T {
+    KeyboardEventType type;
+} KeyboardEvent;
+
+typedef struct EVENT_T {
+    EventSourceFlag source;
+    union {
+        WindowEvent window;
+        KeyboardEvent keyboard;
+    } data;
+} Event;
+
+typedef struct EVENTS_T {
+    ARRAY_FIELDS(Event)
+} Events;
+
+void events_init(const EventInitInfo* info);
 
 void events_poll();
+
+void events_get(EventSource source, Events* events);
 
 void events_uninit();
 
